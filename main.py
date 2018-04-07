0a43da2d80e5d3a96f51e0c40185e8a9266d676e
TrashedBot <35202688+TrashedBot@users.noreply.github.com>
2018-04-07 16:12:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+from flask import request
+from block import *
+
+app = Flask (__name__)
+
+@app.route('/', methods = ['POST','GET'])
+def index():
+
+    if request.method == 'POST':
+        trashedbot_1 = request.form['trashedbot_1']
+        price = request.form['price']
+        trashedbot_2 = request.form['trashedbot_2']
+
+        write_block(name=trashedbot_1, amount=price, to_whom=trashedbot_2)
+        return redirect(url_for('index'))
+
+    return render_template('index.html')
+
+@app.route('/checking', methods = ['GET'])
+def check():
+    results = check_integrity()
+    return render_template('index.html', res_dao = results)
+
+if __name__=='__main__':
+    app.run(debug=True)
